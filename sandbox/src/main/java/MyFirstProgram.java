56e2f266343d00e32dc67d14fa2c79892d2ca34b
virtus80 <virtus80@mail.ru>
2016-04-16 21:04:21 +0300

    Homework2
    - added class Point;
    - commented in main method lecture exercises;
    - added to main method launchng 'distance' method of class Point.

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,7 @@
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-
+/*
     System.out.println("Hello, world!");
 
     Square s = new Square(5);
@@ -9,6 +9,9 @@ public class MyFirstProgram {
 
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+  */
+    Point p1 = new Point(4, 3);
+    Point p2 = new Point(8, 5);
+    System.out.println("Растояние между точками = " + p1.distance(p2));
   }
-
 }
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Sasha on 16.04.2016.
+ */
+public class Point {
+  public double x;
+  public double y;
+
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance(Point p1){
+    return Math.sqrt(Math.pow((p1.x - this.x), 2 ) + Math.pow(p1.y - this.y, 2 ));
+  }
+}
