57c4090c96727de357a70ef9d33a419c6771ebec
virtus80 <virtus80@mail.ru>
2016-04-16 20:44:31 +0300

    Lecture1
    - added classes Square and Rectangle;
    - changed formatting settings;

---
+++ sandbox/src/main/java/Rectangle.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Sasha on 16.04.2016.
+ */
+public class Rectangle {
+
+  public double a;
+  public double b;
+
+  public Rectangle(double a, double b){
+    this.a = a;
+    this.b = b;
+  }
+
+  public double area(){
+    return this.a*this.b;
+  }
+}
---
+++ sandbox/src/main/java/Square.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Sasha on 16.04.2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+
+  public double area(){
+    return this.l*this.l;
+  }
+}
