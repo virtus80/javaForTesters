5194206848d2a6a08090897952b371cd0d443764
virtus80 <virtus80@mail.ru>
2016-04-21 23:39:30 +0300

    NavHelper
    - added NavigationHelper class to previous commit

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Sasha on 21.04.2016.
+ */
+public class NavigationHelper extends HelperBase {
+
+  public NavigationHelper(FirefoxDriver wd) {
+    super(wd);
+  }
+
+  public void gotoGroupPage() {
+    click(By.linkText("groups"));
+  }
+
+}
