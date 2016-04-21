693a03da7dbd26652f3d990214ed6048d75451ea
virtus80 <virtus80@mail.ru>
2016-04-21 23:38:41 +0300

    Group helpers
    - added helper classes

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,42 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Sasha on 21.04.2016.
+ */
+public class ApplicationManager {
+
+  FirefoxDriver wd;
+
+  private SessionHelper sessionHelper;
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
+
+
+  public void init() {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    groupHelper = new GroupHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    sessionHelper = new SessionHelper(wd);
+    sessionHelper.login("admin", "secret");
+  }
+
+
+
+  public void stop() {
+    wd.quit();
+  }
+
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
+  }
+
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Sasha on 21.04.2016.
+ */
+public class SessionHelper extends HelperBase{
+
+  public SessionHelper(FirefoxDriver wd) {
+    super(wd);
+  }
+
+  public void login(String username, String password) {
+    type(By.name("user"), username);
+    type(By.name("pass"), password);
+   click(By.xpath("//form[@id='LoginForm']/input[3]"));
+  }
+}
