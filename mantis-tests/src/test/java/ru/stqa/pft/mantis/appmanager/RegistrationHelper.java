9c42e56ac503a2488d32a888c62fcf8106362398
virtus80 <virtus80@mail.ru>
2016-05-29 22:15:38 +0300

    Login test
    - added direct interaction with server;
    - added lazy initialization of helpers

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Sasha on 29.05.2016.
+ */
+public class RegistrationHelper {
+  private final ApplicationManager app;
+  private WebDriver wd;
+
+  public RegistrationHelper(ApplicationManager app) {
+    this.app = app;
+    wd = app.getDriver();
+  }
+
+  public void start(String username, String email) {
+    wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+  }
+
+}
