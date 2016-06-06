6b383dbbf871212ccdde1d7de5c1fdd3b3ad4e38
virtus80 <virtus80@mail.ru>
2016-06-06 23:16:41 +0300

    SOAP interaction with Mantis
    - added soap tests for get projects and create issue

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -26,6 +26,7 @@ public class ApplicationManager {
   private MailHelper mailHelper;
   private DbHelper dbHelper;
   private JamesHelper jamesHelper;
+  private SoapHelper soapHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -79,6 +80,13 @@ public class ApplicationManager {
       jamesHelper = new JamesHelper(this);
     }
     return jamesHelper;
+  }
+
+  public SoapHelper soap() {
+    if (soapHelper == null) {
+      soapHelper = new SoapHelper(this);
+    }
+    return soapHelper;
   }
 
   public DbHelper db() { return dbHelper; }
---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.soap;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Sasha on 06.06.2016.
+ */
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIp() {
+    new GeoIPService().getGeo
+  }
+}
